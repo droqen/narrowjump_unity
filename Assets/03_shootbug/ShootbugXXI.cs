c133d9b7bc400541e12e4d5e1cf7fda382c4fb8a
droqen <hi@droqen.com>
2019-09-02 21:22:36 -0400

    added more to "shootbug", sprites in but no animation

---
+++ Assets/03_shootbug/ShootbugXXI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+using navdi3;
+using navdi3.tiled;
+
+public class ShootbugXXI : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TextAsset testLevel;
+    public TiledLoader loader { get { return GetComponent<TiledLoader>(); } }
+    public BankLot banks { get { return GetComponent<BankLot>(); } }
+    public SpriteLot sprites { get { return GetComponent<SpriteLot>(); } }
+
+    public EntityLot players { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = EntityLot.NewEntLot("players");
+
+        loader.SetupTileset(sprites,
+            solidTileIds: new int[] { 3 },
+            spawnTileIds: new int[] { 1 });
+        loader.PlaceTiles(loader.Load(testLevel), tilemap, SpawnTileId);
+    }
+
+    void SpawnTileId(int tileId, Vector3Int tilePos)
+    {
+        Vector2 entPos = tilemap.layoutGrid.GetCellCenterWorld(tilePos);
+        switch(tileId)
+        {
+            case 1: banks["marine"].Spawn(players.transform, entPos); break;
+        }
+    }
+}
---
+++ Assets/03_shootbug/shootbug_tiledset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="shootbug_tileset" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="sheet.png" width="160" height="160"/>
+</tileset>
